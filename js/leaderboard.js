991f35dfb4e0768c533d143495e86805aad7c09f
Aviralcodes29 <281214379+aviralcodes29@users.noreply.github.com>
2026-06-13 15:11:07 +0530

    update

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,5 +1,4 @@
-const API_URL =
-"http://127.0.0.1:8000";
+const API_URL = "https://language-learning-backend-zstx.onrender.com";
 
 const leaderboardBody =
 document.getElementById(
--- js/lessons.js
+++ js/lessons.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://language-learning-backend-zstx.onrender.com";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
